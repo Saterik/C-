656d2255b4578b9fe375b25098d28e9a4e4d5d82
warfa@LAPTOP-BGKII3AT <warfacesdr@mail.ru>
2022-12-16 23:56:52 +0800

    sum v1.2 Генератор случайных чисел

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿double numberA = 43;
-double numberB = 5;
+﻿double numberA = new Random().Next(1,10); // 1 2 3 4...9
+double numberB = new Random().Next(1,10);
 Console.WriteLine(numberA / numberB);
 
 
